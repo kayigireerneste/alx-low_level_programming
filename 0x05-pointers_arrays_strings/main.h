bcabe2bf119ad22955a9a464e0deb711e437c456
Kayigire Erneste <85900137+kayigireerneste@users.noreply.github.com>
2023-03-28 20:40:04 +0200

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <unistd.h>
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,3 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif
