2d481d8b1b98bff0f871912b152dea6b53cc89db
kayigireerneste <kayigireerneste@gmail.com>
2023-03-30 23:00:44 +0200

    error fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int destlen = 0;
-	int srclen = 0;
 	int i;
+	int j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-		destlen++;
-	for (i = 0; src[i] != '\0'; i++)
-		srclen++;
-	for (i = 0; i < n; i++)
-		dest[destlen + i] = src[i];
+		;
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	for (j = 0; j < n; j++)
+		dest[i] = '\0';
 	return (dest);
 }
