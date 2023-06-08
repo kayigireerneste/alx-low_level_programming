e25bbecb806584e82d9dd1b77d3991f89c34269e
kayigireerneste <kayigireerneste@gmail.com>
2023-06-08 14:01:28 +0200

    error corrected

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
