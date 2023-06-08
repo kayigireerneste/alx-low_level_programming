8c99890a86b86071195b63149262276bd4b2f087
kayigireerneste <kayigireerneste@gmail.com>
2023-06-08 15:59:27 +0200

    file corrected

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
  */
 void print_times_table(int n)
 {
-	int i, j, product;
+	int i, j, res;
 
 	if (!(n > 15 || n < 0))
 	{
@@ -14,32 +14,32 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				product = (i * j);
+				res = (i * j);
 				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
-				if (product < 10 && j != 0)
+				if (res < 10 && j != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((product % 10) + '0');
+					_putchar((res % 10) + '0');
 				}
-				else if (product >= 10 && product < 100)
+				else if (res >= 10 && res < 100)
 				{
 					_putchar(' ');
-					_putchar((product / 10) + '0');
-					_putchar((product % 10) + '0');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
 				}
-				else if (product >= 100 && j != 0)
+				else if (res >= 100 && j != 0)
 				{
-					_putchar((product / 100) + '0');
-					_putchar((product / 10) % 10 + '0');
-					_putchar((product % 10) + '0');
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
 				}
 				else
-					_putchar((product % 10) + '0');
+					_putchar((res % 10) + '0');
 			}
 			_putchar('\n');
 		}
